fe984136197f9f4e89154ae6e19fc55e95868715
Marc Lundgren <marclundgren2.0@gmail.com>
2014-12-24 08:17:58 -0800

    fuzzy into a functor

--- fuzzy.js
+++ fuzzy.js
@@ -4,14 +4,15 @@
 
 var _ = require('lodash');
 
-var fuzzy = function(items ,query) {
-  query     = query.toLowerCase();
+var fuzzy = function(items, key) {
+  return function (query) {
+    query     = query.toLowerCase();
+    return _.filter(items, function(item) {
+      var term = item[key].toLowerCase();
 
-  return _.filter(items, function(item) {
-    var title = item.title.toLowerCase();
-
-    return _.contains(title, query);
-  });
+      return _.contains(term, query);
+    });
+  };
 };
 
 module.exports = fuzzy;
